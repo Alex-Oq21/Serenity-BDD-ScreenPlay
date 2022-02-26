0a042330a8ae7cc759feb8300acb2c1e0c2455ac
Alex-Oq21 <omaroquendo221@gmail.com>
2022-02-25 19:15:00 -0500

    Using more Questions

---
+++ src/main/java/questions/GetUserQuestion.java
@@ -0,0 +1,13 @@
+package questions;
+
+import models.users.User;
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+public class GetUserQuestion implements Question {
+    @Override
+    public User answeredBy(Actor actor) {
+        return SerenityRest.lastResponse().as(User.class);
+    }
+}
--- src/test/java/SerenityInitTests.java
+++ src/test/java/SerenityInitTests.java
@@ -1,16 +1,19 @@
 
 import Task.GetUser;
+import models.users.Datum;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import questions.GetUserQuestion;
 import questions.ResponseCode;
 
 import static com.google.common.base.Predicates.equalTo;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 @RunWith(SerenityRunner.class)
 public class SerenityInitTests {
@@ -20,12 +23,22 @@ public class SerenityInitTests {
     public void getUsers(){
         Actor alex = Actor.named("Alex");
         alex.whoCan(CallAnApi.at(apiUrl));
-        alex.attemptsTo(GetUser.frompage(2));
+        alex.attemptsTo(GetUser.frompage(1));
         //assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
 
         alex.should(
                 seeThat("The status code is ", ResponseCode.was(), equalTo(200))
+        );
 
+        Datum user = new GetUserQuestion().answeredBy(alex).getData().stream().filter(x -> x.getId() == 1)
+                .findFirst().orElse(null);
+
+        alex.should(
+                seeThat("The user exist", act -> user, notNullValue())
+        );
+
+        alex.should(
+                seeThat("The user Email is", act -> user.getEmail(), equalTo("george.bluth@reqres.in"))
         );
     }
 }
