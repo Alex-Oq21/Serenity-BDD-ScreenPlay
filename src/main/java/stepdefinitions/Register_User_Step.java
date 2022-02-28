40b1b686c878270b3735d2b4872c44799afc23eb
Alex-Oq21 <omaroquendo221@gmail.com>
2022-02-28 18:22:17 -0500

    The first step is added

---
+++ src/main/java/stepdefinitions/Register_User_Step.java
@@ -0,0 +1,22 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Register_User_Step {
+    @Given("Julian es un cliente que quiere poder administrar sus productos bancarios")
+    public void Julian_es_un_cliente_que_quiere_poder_administrar_sus_productos_bancarios(){
+
+    }
+
+    @When("el envia la informacion requerida para el registro")
+    public void el_envia_la_informacion_requerida_para_el_registro(){
+
+    }
+
+    @Then("el debe obtener una cuenta virtual para poder ingresar cuando lo requiera")
+    public void el_debe_obtener_una_cuenta_virtual_para_poder_ingresar_cuando_lo_requiera(){
+
+    }
+}
